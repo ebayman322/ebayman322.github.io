2d78e7997778fa388e571a80d269c5ae01479021
ebayman322 <49350567+ebayman322@users.noreply.github.com>
2023-04-19 14:47:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,33 @@
     </ul>
 </section>
 <!-- ... -->
+
+// Add this code to your existing "script.js" file
+
+const BROWSE_PROMPTS_API_URL = 'https://your-backend-server.com/api/browse-prompts';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if the current page is the "Browse Prompts" page
+    if (document.querySelector('#prompts-list')) {
+        fetchPrompts();
+        addFilterListeners();
+    }
+});
+
+function fetchPrompts() {
+    fetch(BROWSE_PROMPTS_API_URL)
+        .then(response => response.json())
+        .then(prompts => displayPrompts(prompts))
+        .catch(error => console.error('Error fetching prompts:', error));
+}
+
+function displayPrompts(prompts) {
+    const promptsList = document.querySelector('#prompts-list');
+    promptsList.innerHTML = '';
+
+    prompts.forEach(prompt => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <h3>${prompt.title}</h3>
+            <p>${prompt.description}</p>
+            <p
