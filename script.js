b0a0e614c2f255749cf031dd792e821a25930fb6
ebayman322 <49350567+ebayman322@users.noreply.github.com>
2023-04-19 14:40:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+<!-- ... -->
+<section>
+    <h2>Featured Prompts</h2>
+    <ul id="featured-prompts">
+        <!-- Prompts will be added here dynamically -->
+    </ul>
+</section>
+
+<section>
+    <h2>Featured Creations</h2>
+    <ul id="featured-creations">
+        <!-- Creations will be added here dynamically -->
+    </ul>
+</section>
+<!-- ... -->
